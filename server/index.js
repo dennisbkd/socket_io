4db555ee4be77d0221fa30c61871e392e5faf325
dennisbkd <rasguidorondalesdennis@gmail.com>
2025-03-07 18:51:34 -0400

    implementando socket.io

--- server/index.js
+++ server/index.js
@@ -1,14 +1,37 @@
 import express from 'express'
 import logger from 'morgan'
 
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
 const port = process.env.PORT ?? 3000
 
 const app = express()
-
-app.get('/', (req, res) => {
-  res.send('<h1>esto es el chat</h1>')
+const server = createServer(app)
+const io = new Server(server, {
+  connectionStateRecovery: {
+    // aqui van los segundo o minutos de espera
+  }
 })
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('a user has connected!')
+
+  socket.on('disconnect', () => {
+    console.log('an user has disconnected')
+  })
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg)
+  })
+})
+
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/client/index.html')
+})
+
+server.listen(port, () => {
   console.log(`server running on port ${port}`)
 })
